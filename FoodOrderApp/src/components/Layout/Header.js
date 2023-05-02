573d0b9501e87fbe37b336fcc1fc414c2dd799bf
iluviien <stefanica695@gmail.com>
2023-05-02 20:27:10 +0000

    add

--- FoodOrderApp/src/components/Layout/Header.js
+++ FoodOrderApp/src/components/Layout/Header.js
@@ -8,7 +8,7 @@ const Header = props => {
     <Fragment>
       <header className={classes.header}>
         <h1>Darcy's Delicious Meathouse</h1>
-        <HeaderCardButton />
+        <HeaderCardButton onClick={props.onShowCart} />
       </header>
       <div className={classes["main-image"]}>
         <img src={HeaderBanner} alt="header banner" />
