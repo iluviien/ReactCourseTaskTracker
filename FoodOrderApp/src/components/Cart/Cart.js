7e4a3a7174e8218beefd99e8cb506de81401de65
iluviien <stefanica695@gmail.com>
2023-05-03 18:56:44 +0000

    add this shizz

--- FoodOrderApp/src/components/Cart/Cart.js
+++ FoodOrderApp/src/components/Cart/Cart.js
@@ -2,13 +2,25 @@ import { useContext } from "react";
 import CartContext from "../../store/cart-context";
 import Modal from "../UI/Modal";
 import classes from "./Cart.module.css";
+import CartItem from "./CartItem";
 function Cart(props) {
   const cartCtx = useContext(CartContext);
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+  const hasItems = cartCtx.items.length > 0;
+
+  const cartItemRemoveHandler = id => {};
+  const cartItemAddHandler = id => {};
   const cartItems = (
     <ul className={classes["cart-items"]}>
       {cartCtx.items.map(item => (
-        <li>{item.name}</li>
+        <CartItem
+          key={item.id}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+          onRemove={cartItemRemoveHandler.bind(null, item.id)}
+          onAdd={cartItemAddHandler.bind(null, item)}
+        />
       ))}
     </ul>
   );
@@ -23,7 +35,7 @@ function Cart(props) {
         <button className={classes["button--alt"]} onClick={props.onClose}>
           Close
         </button>
-        <button className={classes.button}>Order</button>
+        {hasItems && <button className={classes.button}>Order</button>}
       </div>
     </Modal>
   );
