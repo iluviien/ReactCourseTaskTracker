793e2a25f99fc26ddb548ce7fcce1c622cb913c2
iluviien <stefanica695@gmail.com>
2023-05-12 08:18:48 +0000

    add

--- CustomReactHooks/src/hooks/use-counter.js
+++ CustomReactHooks/src/hooks/use-counter.js
@@ -1,13 +1,17 @@
 import { useEffect, useState } from "react";
 
-function useCounter() {
+function useCounter(forwards = true) {
   const [counter, setCounter] = useState(0);
   useEffect(() => {
     const interval = setInterval(() => {
-      setCounter(prevCounter => prevCounter + 1);
+      if (forwards) {
+        setCounter(prevCounter => prevCounter + 1);
+      } else {
+        setCounter(prevCounter => prevCounter - 1);
+      }
     }, 1000);
     return () => clearInterval(interval);
-  }, []);
+  }, [forwards]);
   return counter;
 }
 export default useCounter;
